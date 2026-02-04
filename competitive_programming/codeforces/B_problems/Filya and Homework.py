163aa4af6101f83ed1dd58db3b18a0c5640fc30f
Aminpatra <aminpatranew@gmail.com>
2026-02-04 15:50:09 +0400

    Solve 'Filya and Homework' problem

---
+++ competitive_programming/codeforces/B_problems/Filya and Homework.py
@@ -0,0 +1,19 @@
+# https://codeforces.com/contest/714/problem/B
+# Topic: Implementation/Sorting
+# Rating: 1200
+
+# Time took to solve problem is: 40 min.
+# Solved in Third try.
+# AI used ? "Yes" 'for a small hint after 30 min thinking' 
+
+int(input())
+arr=sorted(list(set(map(int,input().split()))))
+if len(arr) > 3: print("NO")
+elif len(arr) == 1 or len(arr) == 2: print("YES")
+else: 
+  if (arr[2]-arr[1] == arr[1]-arr[0]):
+    print("YES")
+  else: print("NO")
+
+# Lessons: to make all elements of an array equal by choosing an x integer
+# this x can be subtracted or added only once or we leave the element, then they must from an arithmetic sequence
