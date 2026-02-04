93812262f9b32e1c2e5f5140855dd1f81515db54
Aminpatra <aminpatranew@gmail.com>
2026-02-04 17:12:40 +0400

    Solve 'Product' problem A

---
+++ competitive_programming/codeforces/A_problems/Product.py
@@ -0,0 +1,17 @@
+# https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=9
+# Topic: implementation
+# Rating: A
+
+# Time took to solve problem is: 5 min.
+# Solved in First try.
+# AI used ? NO
+
+try:
+  while (True):
+    x=int(input())
+    y=int(input())
+    print(x * y)
+except:
+  pass
+
+# Lessons: using try and except for unknown number of inputs
