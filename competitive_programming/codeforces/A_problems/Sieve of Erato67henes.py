512a92dd8b998a2a70418fc09654b4e8773de926
Aminpatra <aminpatranew@gmail.com>
2026-02-15 20:13:40 +0400

    Solve 'Sieve of Erato67henes' A problem in contest

---
+++ competitive_programming/codeforces/A_problems/Sieve of Erato67henes.py
@@ -0,0 +1,12 @@
+# https://codeforces.com/contest/2195/problem/A
+# Topic: Unknown
+# Rating: A: Unknown
+
+# Time took to solve problem is: 15 min.
+# Solved in Third try.
+# AI used ? NO
+
+for _ in range(int(input())):
+  n=int(input())
+  nums=list(map(int,input().split()))
+  print('YES' if 67 in nums else "NO")
