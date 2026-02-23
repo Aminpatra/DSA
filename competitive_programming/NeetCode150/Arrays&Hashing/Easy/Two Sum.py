202c5741d0b860555ccaa17da2b38b29aa7a8534
Aminpatra <aminpatranew@gmail.com>
2026-02-23 23:06:29 +0400

    Solve 'Two Sum' Easy problem

---
+++ competitive_programming/NeetCode150/Arrays&Hashing/Easy/Two Sum.py
@@ -0,0 +1,13 @@
+
+
+
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    hash_map = dict()
+    for i in range(len(nums)):
+      comp = target - nums[i]
+      if comp in hash_map: 
+        return [hash_map[comp], i]
+      hash_map[nums[i]] = i
+    return []
+  
